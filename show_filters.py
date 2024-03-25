01a6da077596ce7baba6bbb79df2ea3cbc4a7197
mattcraigie <matthewcraigie1@gmail.com>
2024-03-25 17:58:53 +1000

    Adding initial filters storing

--- show_filters.py
+++ show_filters.py
@@ -4,8 +4,35 @@ import matplotlib.pyplot as plt
 from scattering_transform.filters import ClippedMorlet
 import yaml
 import argparse
-from models import NFSTRegressor
 
+def compare_filters(model):
+
+    num_scales = model.filters.num_scales
+
+
+    filters_final = model.filters.filters
+
+    model.filters.load_state_dict(model.initial_filters_state)
+    model.filters.update_filters()
+    filters_initial = model.filters.filters
+
+    fig, axes = plt.subplots(nrows=num_scales, ncols=6, figsize=(12, 9), dpi=100)
+
+    for j in range(num_scales):
+        # plot the final filters, and then the difference between the final and initial filters
+        filt_k0 = filters_final[j][0]
+        filt_x0 = torch.fft.fft2(filt_k0).abs()
+        filt_k1 = filters_initial[j][0]
+        filt_x1 = torch.fft.fft2(filt_k1).abs()
+
+        axes[j, 0].imshow(filt_k1)
+        axes[j, 1].imshow(filt_x1)
+        axes[j, 2].imshow(filt_k1 - filt_k0)
+        axes[j, 3].imshow(filt_x1 - filt_x0)
+        axes[j, 4].imshow(filt_k1 - filt_k1.flip(0))
+        axes[j, 5].imshow(filt_x1 - filt_x1.flip(0))
+
+    plt.savefig()
 
 def show_filters(model, save_dir, morlet_diff=False):
 
@@ -49,22 +76,22 @@ def show_filters(model, save_dir, morlet_diff=False):
     plt.savefig(os.path.join(save_dir, 'filters.png'))
 
 
-if __name__ == '__main__':
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--model_save_path', type=str, required=True)
-    parser.add_argument('--config_path', type=str, required=True)
-    parser.add_argument('--save_dir', type=str, required=True)
-    parser.add_argument('--morlet_diff', action='store_true')
-    args = parser.parse_args()
-
-    # load config yaml
-    config_path = os.path.join(args.config_path, 'config.yaml')
-    with open(config_path, 'r') as f:
-        config = yaml.safe_load(f)
-
-    model = NFSTRegressor(**config['analysis_kwargs']['model_kwargs'])
-    model.load_state_dict(torch.load(args.model_save_path))
-
-    # show filters
-    show_filters(model, args.save_dir, morlet_diff=args.morlet_diff)
+# if __name__ == '__main__':
+#
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument('--model_save_path', type=str, required=True)
+#     parser.add_argument('--config_path', type=str, required=True)
+#     parser.add_argument('--save_dir', type=str, required=True)
+#     parser.add_argument('--morlet_diff', action='store_true')
+#     args = parser.parse_args()
+#
+#     # load config yaml
+#     config_path = os.path.join(args.config_path, 'config.yaml')
+#     with open(config_path, 'r') as f:
+#         config = yaml.safe_load(f)
+#
+#     model = NFSTRegressor(**config['analysis_kwargs']['model_kwargs'])
+#     model.load_state_dict(torch.load(args.model_save_path))
+#
+#     # show filters
+#     show_filters(model, args.save_dir, morlet_diff=args.morlet_diff)
