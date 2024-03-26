47ddbe0d6e70cfdeed5322b2a36464ad5fa0184f
mattcraigie <matthewcraigie1@gmail.com>
2024-03-26 11:48:47 +1000

    Adding compare filters with morlet init diff

--- show_filters.py
+++ show_filters.py
@@ -55,9 +55,9 @@ def compare_filters(model, save_dir):
     for j in range(num_scales):
         # plot the final filters, and then the difference between the final and initial filters
         filt_k0 = filters_final[j][0].cpu().detach()
-        filt_x0 = torch.fft.fft2(filt_k0).abs()
+        filt_x0 = fts(torch.fft.fft2(filt_k0).abs())
         filt_k1 = filters_initial[j][0].cpu().detach()
-        filt_x1 = torch.fft.fft2(filt_k1).abs()
+        filt_x1 = fts(torch.fft.fft2(filt_k1).abs())
 
         filt_k1_symm = filt_k1.clone()
         filt_k1_symm[1:] = filt_k1_symm[1:] - filt_k1_symm[1:].flip(0)
@@ -65,9 +65,9 @@ def compare_filters(model, save_dir):
         filt_x1_symm = filt_x1.clone()
         filt_x1_symm[1:] = filt_x1_symm[1:] - filt_x1_symm[1:].flip(0)
 
-        axes[0, j].imshow(fts(filt_x1))
-        axes[1, j].imshow(fts(filt_x1 - filt_x0))
-        axes[2, j].imshow(fts(filt_x1_symm))
+        axes[0, j].imshow(filt_x1)
+        axes[1, j].imshow(filt_x1 - filt_x0)
+        axes[2, j].imshow(filt_x1_symm)
 
         for ax in axes.flatten():
             ax.set_xticks([])
