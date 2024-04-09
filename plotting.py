0f6e48fa0c0ff9beb14850b61a0f7c6c77567d53
mattcraigie <matthewcraigie1@gmail.com>
2024-04-09 23:19:24 +1000

    updating show_filters.py

--- plotting.py
+++ plotting.py
@@ -333,7 +333,7 @@ def plot_morlet_filters():
     size, J, L = 32, 3, 8
     morlet = ClippedMorlet(size, J, L)
 
-    k = morlet.filters[1][2]
+    k = morlet.filters[1][-1]
     x = torch.fft.fftshift(torch.fft.fft2(k))
     k = torch.fft.fftshift(k)
 
